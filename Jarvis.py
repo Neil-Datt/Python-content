92d7661eae9e5ebbfeb8bf084f90249d0df0bc39
Neil-Datt <65217520+Neil-Datt@users.noreply.github.com>
2020-05-12 01:29:41 -0700

    Add files via upload

---
+++ Jarvis.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
+
+print("Hi, I am Jarvis. Your Personal assistant")
+print("How may I help you?")
+
+def Who_invented_bulb():
+    print("According to Live Science. Aug 17, 2017 - The first practical incandescent light bulb Edison and his team of researchers in Edison's laboratory in Menlo Park, N.J., tested more than 3,000 designs for bulbs between 1878 and 1880. In November 1879, Edison filed a patent for an electric lamp with a carbon filament.")
+    print("For more information = https://www.livescience.com/43424-who-invented-the-light-bulb.html")
+
+def Who_invented_computer():
+    print("According to wekipedia, Charles Babbage, an English mechanical engineer and polymath, originated the concept of a programmable computer. Considered the 'father of the computer', he conceptualized and invented the first mechanical computer in the early 19th century.")
+    print("For more info = https://en.wikipedia.org/wiki/Computer")
+
+def What_is_a_blackhole():
+    print("According to National American Space Agency,(NASA), Aug 21, 2018 - A black hole is a place in space where gravity pulls so much that even light can not get out. The gravity is so strong because matter has been squeezed into a tiny space. This can happen when a star is dying. Because no light can get out, people can't see black holes.")
+    print("https://www.nasa.gov/audience/forstudents/k-4/stories/nasa-knows/what-is-a-black-hole-k4.html")
+def What_can_you_do():
+    print("I can tell answers of queries and Tell the time.")
+    print("Now the time is")
+    print("Current Time =", current_time)
+def Shut_down():
+    print("If you press enter, the program will close.")
+    print("Thank You")
+
+
+     
